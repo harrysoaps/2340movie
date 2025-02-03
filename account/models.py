aa5e1854282f5616db9df5760df69015f126a220
PruthakPatel1 <pruthak116@gmail.com>
2025-02-03 00:27:40 -0500

    Initialized Account, Register, and Login views

--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.contrib import admin
 
 
+
 class account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=20, unique=True)
--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 
+
 class AccountView(View):
     def get(self, request):
         return
