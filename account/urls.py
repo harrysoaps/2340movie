83e4642ee57be72fcc802b5b41d36ee5f79bed06
PruthakPatel1 <pruthak116@gmail.com>
2025-02-07 18:05:04 -0500

    Created about page and the associated url path and made sure it was accessible to user. Also did the same thing for Home page. Uploaded background.jpg for as a start to an improved IU. Created a base template so we can have a scaffolding for all url paths we want to make. With these changes, Users now have a functional log in and register page that allows them to enter their information.

--- account/urls.py
+++ account/urls.py
@@ -4,10 +4,8 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('register/', views.Register, name='account.register'),
-    path('login/', views.Login, name='account.login'),
-
-    path('logout/', views.logout, name='account.logout'),
+    path('register/', views.register, name='account.register'),
+    path('login/', views.login, name='account.login'),
 
 
 
