5f670c9f50da2805ab2a2dda4532b883bd07b449
PruthakPatel1 <pruthak116@gmail.com>
2025-02-07 18:58:49 -0500

    Updated the About page to make it more aesthetically appealing. I added a GT image (about.jpg) as a place holder for any designs. I updated home/views.py to reflect the reformatting of the about page.

--- home/views.py
+++ home/views.py
@@ -5,5 +5,7 @@ def index(request):
     return render(request, 'home/index.html')
 
 def about(request):
-    return render(request, 'home/about.html')
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html',{'template_data':template_data})
 
